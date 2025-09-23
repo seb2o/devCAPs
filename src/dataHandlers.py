c981fae448f0524282722f57872092a47e3f69c3
menbuas <menbuas69@gmail.com>
2025-09-23 19:46:09 +0200

    moved pretty print of dataset dict to datahander class

--- src/dataHandlers.py
+++ src/dataHandlers.py
@@ -79,6 +79,19 @@ class Dhcp3Fmri:
             else:
                 return transform_list[0]
 
+    @staticmethod
+    def pretty_print_dataset(dataset_dict):
+        for subject_path, sessions_dict in dataset_dict.items():
+            print(f"Subject: {subject_path.name}")
+            for session_path, session_data in sessions_dict.items():
+                print(f"  Session: {session_path.name}")
+                bolds = [b.name for b in session_data['bolds']]
+                transform = session_data['transform'].name
+                print(f"    Transform: {transform}")
+                for runid, run in enumerate(bolds):
+                    print(f"    Run {runid}: {run}")
+
+
     def get_bolds_paths_from_session_path(self, ses_path):
         func_dir = ses_path / "func"
         if not func_dir.is_dir(): raise ValueError(f"func directory not found in {func_dir}")
