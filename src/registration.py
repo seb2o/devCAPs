4e0bd71521c9fea4ba1b240840cb56276275c81e
Sebastien Pierre Marie Boo <boo@miplabsrv6.epfl.ch>
2025-09-24 11:12:01 +0200

    file calling the handler to perform registration

---
+++ src/registration.py
@@ -0,0 +1,44 @@
+import dataHandlers
+
+def main():
+    subs_to_keep = [
+        "sub-CC00119XX12",
+        "sub-CC00330XX09",
+        "sub-CC00702AN09",
+        "sub-CC00980XX16",
+        "sub-CC00134XX11",
+        "sub-CC00340XX11",
+        "sub-CC00702BN09",
+        "sub-CC00987XX23",
+        "sub-CC00168XX12",
+        "sub-CC00342XX13",
+        "sub-CC00731XX14",
+        "sub-CC01023XX09"
+    ]
+    template = "extdhcp40wk_lowres"
+    datadir = "/home/boo/kebiri/rel3_dhcp_fmri_pipeline/"
+    derivativedir = "/home/boo/capslifespan/data/derivatives/"
+    templates_paths={
+        "extdhcp40wk_lowres":"/home/boo/capslifespan/data/templates/extdhcp40wk_lowres.nii.gz",
+    }
+
+    dataset = dataHandlers.Dhcp3Fmri(
+        root=datadir,
+        derivative_root=derivativedir,
+        templates_paths=templates_paths,
+    )
+
+    datastructure = dataset.get_raw_bolds_and_transforms_paths(
+        template_name=template,
+        subject_filter=lambda subject: subject.name in subs_to_keep,
+        session_filter=lambda session: True,
+    )
+
+    dataset.pretty_print_dataset(datastructure)
+    dataset.normalize(
+        template_name=template,
+        subject_filter=lambda subject: subject.name in subs_to_keep,
+        session_filter=lambda session: True
+    )
+if __name__ == "__main__":
+    main()
