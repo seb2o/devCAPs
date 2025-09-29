0b9d0e73cccfc4418d0bffcec950034a92c98977
menbuas <menbuas69@gmail.com>
2025-09-29 15:35:11 +0200

    corrected folder usages in registration.py

--- src/registration.py
+++ src/registration.py
@@ -4,21 +4,22 @@ def main():
 
 
     template = "extdhcp40wk-lowres"
-    datadir = paths.bids_root
+    bids_root = paths.bids_root
+    preprocessed_bolds_path = paths.preprocessed_bolds
     derivative_root=paths.derivatives
     templates_paths = {
         "extdhcp40wk-lowres": paths.ext40Template_lowres
     }
 
     folder = dataHandlers.Dhcp3Fmri(
-        root=datadir,
+        root=bids_root,
         derivative_root=derivative_root,
         templates_paths=templates_paths,
     )
 
     dataset_info = utils.build_dataset_info(
-        bids_root=datadir,
-        bolds_folder=datadir,
+        bids_root=bids_root,
+        bolds_folder=preprocessed_bolds_path,
         template_name=template,
         transform_pattern="from-bold_to-extdhcp40wk",
         transform_extension=".nii.gz",
@@ -26,9 +27,9 @@ def main():
 )
 
     folder.pretty_print_dataset(dataset_info['tree'])
-    folder.normalize(
-            dataset_infos=dataset_info,
-            verbose=2
-     )
+    # folder.normalize(
+    #         dataset_infos=dataset_info,
+    #         verbose=2
+    #  )
 if __name__ == "__main__":
     main()
