d7d63a98dde6ca7c87b74039017ca35c24287197
menbuas <menbuas69@gmail.com>
2025-09-29 15:22:16 +0200

    using pathlib, gather all paths in one file that works anywhere given relative directory structure is maintained

--- src/paths.py
+++ src/paths.py
@@ -1,21 +1,44 @@
-import os.path as op
+from pathlib import Path
 
 atlas_url = "https://git.fmrib.ox.ac.uk/seanf/dhcp-resources/-/blob/master/docs/dhcp-augmented-volumetric-atlas-extended.md"
 
-src = op.dirname(op.abspath(__file__))
-absolute_root = op.dirname(src)
-data = op.join(absolute_root, "data")
 
-ext40Template = op.join(data, "extdhcp40wk.nii.gz")
-ext40Template_lowres = op.join(data, "extdhcp40wk_lowres.nii.gz")
-ext40Parcellation = op.join(data, "extdhcp40wk_parcellation.nii.gz")
-ext40Mask = op.join(data, "extdhcp40wk_mask.nii.gz")
+### REQUIRED STRUCTURE
+# project_root/
+# ├── data/
+# │   ├── templates/
+# │   │   ├── extdhcp40wk.nii.gz
+# │   │   ├── extdhcp40wk_lowres.nii.gz
+# │   │   ├── extdhcp40wk_parcellation.nii.gz
+# │   │   └── extdhcp40wk_mask.nii.gz
+# │   ├── derivatives/
+# │   ├── preprocessed_bolds/
+# │   └── bids_root/
+# └── src/
+#     └── paths.py  <- this file
 
 
-def rel(path):
+
+
+src = Path(__file__).resolve().parent
+absolute_root = src.parent
+data = absolute_root / "data"
+
+templates = data / "templates"
+derivatives = data / "derivatives"
+preprocessed_bolds = data / "preprocessed_bolds"
+bids_root = data / "bids_root"
+
+ext40Template = templates / "extdhcp40wk.nii.gz"
+ext40Template_lowres = templates / "extdhcp40wk_lowres.nii.gz"
+ext40Parcellation = templates / "extdhcp40wk_parcellation.nii.gz"
+ext40Mask = templates / "extdhcp40wk_mask.nii.gz"
+
+
+def rel(path: Path) -> str:
     """
     given an absolute path, returns the relative path starting from the root of the project
     :param path: the path that we want to convert to relative
     :return:
     """
-    return op.relpath(path, absolute_root)
+    return str(path.relative_to(absolute_root))
--- src/registration.py
+++ src/registration.py
@@ -1,36 +1,29 @@
-import utils
-import dataHandlers
+import utils, dataHandlers, paths
 
 def main():
 
 
     template = "extdhcp40wk-lowres"
-    datadir = "/home/boo/kebiri/rel3_dhcp_fmri_pipeline/"
-    derivativedir = "/home/boo/capslifespan/data/derivatives/"
-    templates_paths={
-        "extdhcp40wk-lowres":"/home/boo/capslifespan/data/templates/extdhcp40wk_lowres.nii.gz",
+    datadir = paths.bids_root
+    derivative_root=paths.derivatives
+    templates_paths = {
+        "extdhcp40wk-lowres": paths.ext40Template_lowres
     }
 
     folder = dataHandlers.Dhcp3Fmri(
         root=datadir,
-        derivative_root=derivativedir,
+        derivative_root=derivative_root,
         templates_paths=templates_paths,
     )
 
     dataset_info = utils.build_dataset_info(
         bids_root=datadir,
-        bolds_folder="/home/boo/kebiri/preproc_fMRI_dHCP/",
-        template_name="extdhcp40wk-lowres",
+        bolds_folder=datadir,
+        template_name=template,
         transform_pattern="from-bold_to-extdhcp40wk",
         transform_extension=".nii.gz",
         subject_filter=lambda subject: True
 )
-
-#    datastructure = dataset.get_raw_bolds_and_transforms_paths(
-#        template_name=template,
-#        subject_filter=lambda subject: subject.name in subs_to_keep,
-#        session_filter=lambda session: True,
-#    )
 
     folder.pretty_print_dataset(dataset_info['tree'])
     folder.normalize(
