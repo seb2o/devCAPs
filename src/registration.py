86769151a830795eeda19f0fb28228618218e839
menbuas <menbuas69@gmail.com>
2025-09-29 15:48:56 +0200

    working version of registration

--- src/registration.py
+++ src/registration.py
@@ -28,10 +28,10 @@ def main():
 
     folder.pretty_print_dataset(dataset_info['tree'])
     
-    # folder.normalize(
-    #         dataset_infos=dataset_info,
-    #         verbose=2
-    #  )
+    folder.normalize(
+            dataset_infos=dataset_info,
+            verbose=2
+     )
 
 if __name__ == "__main__":
     main()
