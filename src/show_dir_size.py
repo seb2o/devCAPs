df5c6cd7618369a5495a8dff66159dcb2a6fc84d
menbuas <menbuas69@gmail.com>
2025-10-13 12:39:00 +0200

    faster way to get the fullsize

--- src/show_dir_size.py
+++ src/show_dir_size.py
@@ -5,6 +5,13 @@ import paths
 def main():
     non_preterm_path = paths.derivatives / "non_preterm"
     vol_dirs = sorted(non_preterm_path.glob("sub-*/vols/"))
+
+    n_vol_dirs = len(vol_dirs)
+
+    size_one_vol_dir = sum(f.stat().st_size for f in vol_dirs[0].rglob("*") if f.is_file())
+
+    print(f"estimated size of all vol dirs: {n_vol_dirs * size_one_vol_dir / (1024 ** 3):.2f} GB")
+
     fullsize = 0
     for vol_dir in vol_dirs:
         total_size = sum(f.stat().st_size for f in vol_dir.rglob("*") if f.is_file())
