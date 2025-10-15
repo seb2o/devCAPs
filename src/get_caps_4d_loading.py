e83b303011f43954766542f2e95462b1171e0642
menbuas <menbuas69@gmail.com>
2025-10-15 21:08:52 +0200

    relax num threads for loading subjects

--- src/get_caps_4d_loading.py
+++ src/get_caps_4d_loading.py
@@ -57,7 +57,7 @@ def main(
         retained_frames = []
         times = []
         start_time = perf_counter()
-        with ThreadPoolExecutor(max_workers=os.cpu_count() or 4) as ex:
+        with ThreadPoolExecutor(max_workers=max(os.cpu_count(), 8)) as ex:
 
             futures = [
                 ex.submit(utils.extract_subject_frames, bold_path, gm_mask, seed_mask, t, sel_mode)
