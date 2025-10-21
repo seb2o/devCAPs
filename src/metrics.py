d8cba664fdd73b79f613afc69d8aad9f35e72518
menbuas <menbuas69@gmail.com>
2025-10-21 11:37:40 +0200

    optionnaly show networkX graph for each subject

--- src/metrics.py
+++ src/metrics.py
@@ -60,10 +60,9 @@ def CAPOutDegree(tpm_s):
     np.fill_diagonal(tpm_s, 0)
     return {i:v for i,v in enumerate(np.sum(tpm_s, axis=1))}
 
-def BetweennessCentrality(tpm_s):
+def BetweennessCentrality(tpm_s, show_graph=True):
     tpm_s = np.array(tpm_s, copy=True)
     np.fill_diagonal(tpm_s, 0)
-    # transform higher probabilities to lower distances
 
     n = tpm_s.shape[0]
     G = nx.DiGraph()
@@ -77,6 +76,21 @@ def BetweennessCentrality(tpm_s):
     for i, j, w in zip(rows.tolist(), cols.tolist(), costs.tolist()):
         G.add_edge(i, j, weight=w)
 
+    if show_graph:
+        weights = [100*np.exp(-d['weight']) for (_, _, d) in G.edges(data=True)]
+        # Plot with arrows and width proportional to weight
+        pos = nx.spring_layout(G)
+        nx.draw(
+            G, pos,
+            with_labels=True,
+            node_color='lightblue',
+            node_size=700,
+            width=weights,
+            font_size=10,
+            arrowsize=20,
+            connectionstyle='arc3,rad=0.1'
+        )
+        plt.show()
     # Weighted, directed betweenness centrality on shortest paths
     # normalized=True returns values in [0,1]
     bc = nx.betweenness_centrality(G, k=None, normalized=True, weight="weight", endpoints=False)
