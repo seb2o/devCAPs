2a60b32945b85e38dd02b7c57de4666866ae7206
menbuas <menbuas69@gmail.com>
2025-11-07 10:45:58 +0100

    Revert "change how dfs are deleted to optimize memory"
    This reverts commit 3d6c69171cc2af59612679544dc1c60f9e07ea2d.

--- src/get_caps_4d_loading.py
+++ src/get_caps_4d_loading.py
@@ -74,9 +74,14 @@ def main(
 
     utils.print_memstate(message="Before copying frames: ")
 
-    stacked_frames = np.stack(retained_frames_df['frame'].to_numpy(copy=False))
+    stacked_frames = np.stack(retained_frames_df['frame'].to_numpy(copy=True))
 
-    utils.print_memstate(message="After extracting frames: ")
+    utils.print_memstate(message="After copying frames: ")
+
+    del retained_frames_df
+
+    utils.print_memstate(message="After removing frames: ")
+
 
     # zscore samples to approximate correlation distance with euclidean
     # this is important for kmeans to work well
@@ -131,9 +136,7 @@ def main(
             metric=cluster_dist,
             verbose=2,
         )
-    del retained_frames_df
-    utils.print_memstate(message="After removing frames df: ")
-
+    del stacked_frames
 
     retained_frames_df = pd.read_pickle(savedir / paths.retained_frames_wo_clusters_df_name)
 
