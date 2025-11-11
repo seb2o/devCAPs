47eca628dbd6fb592401c6dd5558697f79559d66
menbuas <menbuas69@gmail.com>
2025-11-11 18:56:20 +0100

    exp param

--- src/dictionnary_learning_CAP.py
+++ src/dictionnary_learning_CAP.py
@@ -174,35 +174,35 @@ if __name__ == "__main__":
     main(
         group_path=paths.sample_derivatives,
         t=15,
-        sel_mode='both',
+        sel_mode='pos',
         optional_path_prefix="",
         load_retained_frames_df=False,
-        n_comps=4,
+        n_comps=40,
         positive_code=True,
         alpha=2.0,
         subject_loading_n_workers=os.cpu_count()//4,
     )
 
-    main(
-        group_path=paths.sample_derivatives,
-        t=15,
-        sel_mode='pos',
-        optional_path_prefix="",
-        load_retained_frames_df=False,
-        n_comps=4,
-        positive_code=True,
-        alpha=1.0,
-        subject_loading_n_workers=os.cpu_count() // 4,
-    )
-
-    main(
-        group_path=paths.sample_derivatives,
-        t=15,
-        sel_mode='pos',
-        optional_path_prefix="",
-        load_retained_frames_df=False,
-        n_comps=4,
-        positive_code=True,
-        alpha=2.0,
-        subject_loading_n_workers=os.cpu_count() // 4,
-    )
+    # main(
+    #     group_path=paths.sample_derivatives,
+    #     t=15,
+    #     sel_mode='pos',
+    #     optional_path_prefix="",
+    #     load_retained_frames_df=False,
+    #     n_comps=4,
+    #     positive_code=True,
+    #     alpha=1.0,
+    #     subject_loading_n_workers=os.cpu_count() // 4,
+    # )
+    #
+    # main(
+    #     group_path=paths.sample_derivatives,
+    #     t=15,
+    #     sel_mode='pos',
+    #     optional_path_prefix="",
+    #     load_retained_frames_df=False,
+    #     n_comps=4,
+    #     positive_code=True,
+    #     alpha=2.0,
+    #     subject_loading_n_workers=os.cpu_count() // 4,
+    # )
