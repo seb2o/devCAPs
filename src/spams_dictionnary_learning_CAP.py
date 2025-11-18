00ade4511b8944e33dc71eac5710afe22030d38e
menbuas <menbuas69@gmail.com>
2025-11-18 18:59:19 +0100

    add possibility of different modes for optimization

--- src/spams_dictionnary_learning_CAP.py
+++ src/spams_dictionnary_learning_CAP.py
@@ -25,10 +25,44 @@ def main(
         subject_loading_n_workers=2,
         n_iters=300,
         positive_atoms=True,
-        n_inits=50
+        n_inits=50,
+        constraint_mode='l0Codes-optimL2Atoms'
 ):
 
-    if positive_code: raise NotImplementedError("SPAMS does not support positive coding only yet.")
+    constraint_modes_mapping = {
+        # 'l1Codes-optimL2Atoms': 0,
+        # 'l2Atoms-optimL1Codes': 1, # mode 2 is weird we dont use it
+        'l0Codes-optimL2Atoms': 3,
+        'l2Atoms-optimL0Codes': 4,
+        'None-optimL2AtomsAlphaL0Codes':5
+    }
+
+    if constraint_mode not in constraint_modes_mapping.keys():
+        raise ValueError(f"constraint_mode must be one of {list(constraint_modes_mapping.keys())}, got {constraint_mode} instead.")
+
+    if constraint_mode in ['l0Codes-optimL2Atoms', 'l2Atoms-optimL0Codes', 'None-optimL2AtomsAlphaL0Codes']:
+        if positive_code:
+            raise NotImplementedError("positive_code=True is not compatible with l0 constraints on codes.")
+
+
+
+    omp_L = None
+    omp_eps = None
+    omp_lambda1 = None
+
+    if constraint_mode == "l0Codes-optimL2Atoms":
+        # omp uses L non zero codes per sample
+        omp_L = alpha
+    elif constraint_mode == "l2Atoms-optimL0Codes":
+        # omp uses eps on the reconstruction error per sample
+        omp_eps = alpha
+    elif constraint_mode == "None-optimL2AtomsAlphaL0Codes":
+        # omp uses lambda1 as weight in the sum of reconstruction error + l0 norm of codes
+        omp_lambda1 = alpha
+
+
+
+
 
     pid = os.getpid()
 
@@ -57,11 +91,12 @@ def main(
         f"{optional_path_prefix}"
         f"SPAMS_DictLr"
         f"_Ncomps-{n_comps}"
+        f"_alpha-{alpha}"
         f"_PosCode-{positive_code}"
         f"_PosAtoms-{positive_atoms}"
+        f"_Constr-{constraint_mode}"
         f"_nIters-{n_iters}"
         f"_nInits-{n_inits}"
-        f"_alpha-{alpha}"
         f"_t-{t}"
         f"_Act-{sel_mode}"
         f"_n-{n_subjs}"
@@ -105,10 +140,6 @@ def main(
     per_seed_results = []
     for init in range(n_inits):
 
-        # dict_inits = np.asfortranarray(np.random.default_rng(seed=init).random(
-        #     size=(reshaped_stacked_frames.shape[0], n_comps),
-        #     dtype=np.double
-        # ))
 
         # select k random frames as initial dictionary
         col_ids = np.random.default_rng(seed=init).choice(
@@ -129,20 +160,22 @@ def main(
             verbose=True,
             iter=n_iters,
             posD=positive_atoms,
-            return_model=False
-        ) # D shape (n_dimensions, n_components) (each column is a component)
+            posAlpha=positive_code,
+            return_model=False,
+        ) # D shape (n_voxels, n_components) (each column is a component)
 
-        # print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Dictionary learned, now computing assignments")
 
         Alpha = spams.omp(
             reshaped_stacked_frames,
             D,
-            L=alpha,
+            L=omp_L,
+            eps=omp_eps,
+            lambda1=omp_lambda1,
             numThreads=subject_loading_n_workers
         )
         #sum_{i=1}^n (1/2)||x_i-Dalpha_i||_2^2
-        # D has shape (n_dimensions, n_components)
-        # Alpha has shape (n_components, n_samples)
+        # D has shape (n_voxels, n_components)
+        # Alpha has shape (n_components, n_frames), a column is the weights for each component for a given frame
         err_matrix = reshaped_stacked_frames - D @ Alpha
         # each column i is the elementwise difference between the sample i
         # and its approximation by linear combination of D with weights Alpha_i
@@ -273,9 +306,10 @@ if __name__ == "__main__":
         positive_code=False,
         alpha=2.0,
         subject_loading_n_workers=os.cpu_count() // 4,
-        n_iters=1, # todo change
-        positive_atoms=False,
-        n_inits=1 # todo change
+        n_iters=1000,
+        positive_atoms=True,
+        n_inits=50,
+        constraint_mode='l0Codes-optimL2Atoms'
     )
 
     main(
@@ -288,8 +322,43 @@ if __name__ == "__main__":
         positive_code=False,
         alpha=2.0,
         subject_loading_n_workers=os.cpu_count() // 4,
-        n_iters=1, # todo change
-        positive_atoms=True,
-        n_inits=1 # todo change
+        n_iters=1000,
+        positive_atoms=False,
+        n_inits=50,
+        constraint_mode='l0Codes-optimL2Atoms'
     )
+
+    main(
+        group_path=paths.sample_derivatives,
+        t=15,
+        sel_mode='pos',
+        optional_path_prefix='',
+        load_retained_frames_df=True,
+        n_comps=4,
+        positive_code=False,
+        alpha=950, # here begigger since contraisnt on l2 norm squared
+        subject_loading_n_workers=os.cpu_count() // 4,
+        n_iters=1000,
+        positive_atoms=True,
+        n_inits=50,
+        constraint_mode='l2Atoms-optimL0Codes'
+    )
+
+    main(
+        group_path=paths.sample_derivatives,
+        t=15,
+        sel_mode='pos',
+        optional_path_prefix='',
+        load_retained_frames_df=True,
+        n_comps=4,
+        positive_code=False,
+        alpha=2.0,
+        subject_loading_n_workers=os.cpu_count() // 4,
+        n_iters=1000,
+        positive_atoms=False,
+        n_inits=50,
+        constraint_mode='None-optimL2AtomsAlphaL0Codes'
+    )
+
+
 
