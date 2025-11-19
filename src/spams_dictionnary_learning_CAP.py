bcb79e9a636094bae66ab0812a430b349c1e12ac
menbuas <menbuas69@gmail.com>
2025-11-19 14:01:51 +0100

    exp params

--- src/spams_dictionnary_learning_CAP.py
+++ src/spams_dictionnary_learning_CAP.py
@@ -312,7 +312,21 @@ def main(
     return savedir
 
 if __name__ == "__main__":
-
+    main(
+        group_path=paths.sample_derivatives,
+        t=15,
+        sel_mode='pos',
+        optional_path_prefix='',
+        load_retained_frames_df=True,
+        n_comps=10,
+        positive_code=False,
+        alpha=2.0,
+        subject_loading_n_workers=os.cpu_count() // 4,
+        n_iters=1000,
+        positive_atoms=True,
+        n_inits=50,
+        constraint_mode='l0Codes-optimL2Atoms'
+    )
 
 
     main(
@@ -324,7 +338,7 @@ if __name__ == "__main__":
         n_comps=4,
         positive_code=False,
         alpha=950, # here begigger since contraisnt on l2 norm squared
-        subject_loading_n_workers=os.cpu_count() // 4,
+        subject_loading_n_workers=1,#os.cpu_count() // 4,
         n_iters=2,
         positive_atoms=True,
         n_inits=4,
@@ -347,21 +361,7 @@ if __name__ == "__main__":
         constraint_mode='None-optimL2AtomsAlphaL0Codes'
     )
 
-    main(
-        group_path=paths.sample_derivatives,
-        t=15,
-        sel_mode='pos',
-        optional_path_prefix='',
-        load_retained_frames_df=True,
-        n_comps=10,
-        positive_code=False,
-        alpha=2.0,
-        subject_loading_n_workers=os.cpu_count() // 4,
-        n_iters=1000,
-        positive_atoms=True,
-        n_inits=50,
-        constraint_mode='l0Codes-optimL2Atoms'
-    )
+
 
 
 
